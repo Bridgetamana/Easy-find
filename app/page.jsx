0f1b970bf2435161b9d57a784344590f28272841
Lilianada <lilibest30.lb@gmail.com>
2024-01-04 01:14:57 +0100

    fixed unauthorized header

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,14 @@
 import React from "react";
 import "./global.scss";
 import LandingPage from "./landing-page";
-// require("dotenv").config();
-// import LandingPage from "./landing-page.jsx/page";
+import { AuthProvider } from "../utils/authContext";
 
 export default function Home() {
   return (
+    <AuthProvider>
     <div className="app">
        <LandingPage/>
     </div>
+    </AuthProvider>
   );
 }
