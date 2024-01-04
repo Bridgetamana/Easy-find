9c8a18b445f7ed6b27fe7d8e910bdbd0c5147629
Lilianada <lilibest30.lb@gmail.com>
2024-01-04 01:22:24 +0100

    fixed unauthorized header

--- components/unauthorized/Footer/index.jsx
+++ components/unauthorized/Footer/index.jsx
@@ -1,99 +1,23 @@
 import React from "react";
 import "./style.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="unauthorized__footer">
-      <div className="footer__top">
-        <div className="footer__top_component">
-          <h3 className="title">For Talents</h3>
-          <ul className="footer__list">
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Browse Jobs
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Browse Companies
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                  Submit Resume
-              </Link>
-            </li>
-          </ul>
-        </div>
-        
-        <div className="footer__top_component">
-          <h3 className="title">For Employers</h3>
-          <ul className="footer__list">
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-              Browse Candidates
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-              Employer Dashboard
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Post Job
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__top_component">
-          <h3 className="title">Legal</h3>
-          <ul className="footer__list">
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Terms of Service
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Privacy Policy
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Cookie Policy
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="footer__top_component">
-          <h3 className="title">Connect</h3>
-          <ul className="footer__list">
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Facebook
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                Twitter
-              </Link>
-            </li>
-            <li className="footer__list_item">
-              <Link href="/" className="footer__link">
-                LinkedIn
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
       <div className="footer__bottom">
          <div className="footer__right">
-           <div className="logo">
-             <h2 className="logo__name">MinuJobs</h2>
-           </div>
-           <ul className="footer__list">
+              <Image
+                src="/assets/images/EasyFind.svg"
+                alt="Logo"
+                sizes="100px"
+                width={100}
+                height={10}
+                layout="fixed"
+                className="logo"
+              />
+           {/* <ul className="footer__list">
              <li className="footer__list_item">
                <a className="footer__link" href="/">
                  About
@@ -114,9 +38,9 @@ export default function Footer() {
                  Contact
                </a>
              </li>
-           </ul>
-         </div>
+           </ul> */}
          <p className="footer__text">&copy; {new Date().getFullYear()} MinuJobs. All rights reserved</p>
+         </div>
        </div>
     </footer>
   );
