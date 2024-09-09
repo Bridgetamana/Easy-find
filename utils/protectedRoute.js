28bd567f866cbfdb9edd81aa5474e97a130c217f
Lilianada <lilibest30.lb@gmail.com>
2024-09-09 23:11:09 +0100

    fix: unable to fix current render issue

--- utils/protectedRoute.js
+++ utils/protectedRoute.js
@@ -2,18 +2,28 @@ import { useContext } from 'react';
 import { AuthContext } from '../context//authContext';
 import { useRouter } from 'next/navigation';
 import secureLocalStorage from "react-secure-storage";
+import { useEffect, useState } from 'react';
+import { useAuth } from '../firebase';
 
-function ProtectedRoute({ children }) {
-  const { user } = useContext(AuthContext);
+const ProtectedRoute = ({ children }) => {
   const router = useRouter();
-  const token = secureLocalStorage.getItem("userToken");
+  const { user } = useAuth(); // Destructure user directly if your context provides it this way
+  const [loading, setLoading] = useState(true);
 
-  if (!user || !token) {
-    router.push("/signin");
-    return null;
+  useEffect(() => {
+    if (!user) {
+      // If the user is not authenticated, redirect to the login page
+      router.push('/login');
+    } else {
+      setLoading(false); // User is authenticated, allow rendering of children
+    }
+  }, [user, router]);
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any other loading indicator
   }
 
-  return children;
-}
+  return <>{children}</>;
+};
 
 export default ProtectedRoute;
