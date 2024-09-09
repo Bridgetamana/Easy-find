3aa2cb626d60adc8f382e35f0004636cb1c0c200
Lilianada <lilibest30.lb@gmail.com>
2024-09-10 00:09:13 +0100

    fix: fixed render issue, was using UserContext in _app.js instead of UserProvider

--- utils/protectedRoute.js
+++ utils/protectedRoute.js
@@ -2,28 +2,18 @@ import { useContext } from 'react';
 import { AuthContext } from '../context//authContext';
 import { useRouter } from 'next/navigation';
 import secureLocalStorage from "react-secure-storage";
-import { useEffect, useState } from 'react';
-import { useAuth } from '../firebase';
 
-const ProtectedRoute = ({ children }) => {
+function ProtectedRoute({ children }) {
+  const { user } = useContext(AuthContext);
   const router = useRouter();
-  const { user } = useAuth(); // Destructure user directly if your context provides it this way
-  const [loading, setLoading] = useState(true);
+  const token = secureLocalStorage.getItem("userToken");
 
-  useEffect(() => {
-    if (!user) {
-      // If the user is not authenticated, redirect to the login page
-      router.push('/login');
-    } else {
-      setLoading(false); // User is authenticated, allow rendering of children
-    }
-  }, [user, router]);
-
-  if (loading) {
-    return <div>Loading...</div>; // Or any other loading indicator
+  if (!user || !token) {
+    router.push("/signin");
+    return null;
   }
 
-  return <>{children}</>;
-};
+  return children;
+}
 
 export default ProtectedRoute;
