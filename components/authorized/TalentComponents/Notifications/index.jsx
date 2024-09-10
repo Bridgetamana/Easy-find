3d18bd760dc72b1dfb4cbc5006daaa508e5c506a
Bridget Amana <amanabridget03@gmail.com>
2024-09-10 14:35:54 +0100

    Add conditional rendering to talent and company notification dropdown for when there are no notification

--- components/authorized/TalentComponents/Notifications/index.jsx
+++ components/authorized/TalentComponents/Notifications/index.jsx
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import styles from './style.module.scss';
-import { MdClose } from 'react-icons/md';
+import React, { useState } from "react";
+import styles from "./style.module.scss";
+import { MdClose } from "react-icons/md";
 
 const notificationsData = [
-  {
-    id: 1,
-    type: 'save',
-    talentName: 'John Doe',
-    jobTitle: 'Software Engineer',
-    date: '2023-08-03',
-  },
-  {
-    id: 2,
-    type: 'apply',
-    talentName: 'Jane Smith',
-    jobTitle: 'Web Developer',
-    date: '2023-09-15',
-  },
+  // {
+  //   id: 1,
+  //   type: 'save',
+  //   talentName: 'John Doe',
+  //   jobTitle: 'Software Engineer',
+  //   date: '2023-08-03',
+  // },
+  // {
+  //   id: 2,
+  //   type: 'apply',
+  //   talentName: 'Jane Smith',
+  //   jobTitle: 'Web Developer',
+  //   date: '2023-09-15',
+  // },
   ,
-  {
-    id: 3,
-    type: 'apply',
-    talentName: 'Jane Smith',
-    jobTitle: 'Web Developer',
-    date: '2023-09-15',
-  },
-  // Add more notifications here as needed
+  // {
+  //   id: 3,
+  //   type: 'apply',
+  //   talentName: 'Jane Smith',
+  //   jobTitle: 'Web Developer',
+  //   date: '2023-09-15',
+  // },
 ];
 
 export default function NotificationTab({ closeNotifications }) {
@@ -40,23 +39,29 @@ export default function NotificationTab({ closeNotifications }) {
         </button>
       </div>
       <div className={styles.notifications__list}>
-        {notifications.map((notification) => (
-          <div key={notification.id} className={styles.notification__item}>
-            <div className={styles.notification__content}>
-              <h2 className={styles.notification__title}>
-                {notification.type === 'save'
-                  ? `${notification.talentName} saved a job post`
-                  : `${notification.talentName} applied to a job post`}
-              </h2>
-              <p className={styles.notification__description}>
-                {notification.type === 'save'
-                  ? `${notification.talentName} saved the job post for ${notification.jobTitle}.`
-                  : `${notification.talentName} applied to the job post for ${notification.jobTitle}.`}
-              </p>
+        {notifications.length === 0 ? (
+          <p className={styles.no_notifications}>
+            Nothing right now. Check back later!
+          </p>
+        ) : (
+          notifications.map((notification) => (
+            <div key={notification.id} className={styles.notification__item}>
+              <div className={styles.notification__content}>
+                <h2 className={styles.notification__title}>
+                  {notification.type === "save"
+                    ? `${notification.talentName} saved a job post`
+                    : `${notification.talentName} applied to a job post`}
+                </h2>
+                <p className={styles.notification__description}>
+                  {notification.type === "save"
+                    ? `${notification.talentName} saved the job post for ${notification.jobTitle}.`
+                    : `${notification.talentName} applied to the job post for ${notification.jobTitle}.`}
+                </p>
+              </div>
+              <p className={styles.notification__date}>{notification.date}</p>
             </div>
-            <p className={styles.notification__date}>{notification.date}</p>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
