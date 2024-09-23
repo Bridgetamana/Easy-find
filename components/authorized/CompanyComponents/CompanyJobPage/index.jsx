962e3c25d885d321a72a6a5b93eef286b03bd7b9
Bridget Amana <amanabridget03@gmail.com>
2024-09-23 01:44:14 +0100

    add style to company post page

---
+++ components/authorized/CompanyComponents/CompanyJobPage/index.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import { getJobIdsFromCompany } from "../../../../firebaseConfig/companyStore";
+import { CiMenuKebab } from "react-icons/ci";
+import styles from "./style.module.scss"; 
+
+const JobPage = () => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [activeDropdown, setActiveDropdown] = useState(false);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const jobData = await getJobIdsFromCompany();
+        setJobs(jobData);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching jobs:", err);
+        setError(err);
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
+  const handleMenuClick = (jobId) => {
+    setActiveDropdown(activeDropdown === jobId ? false : jobId);
+  };
+
+  const handleEdit = () => {
+    console.log("edit button has been clicked");
+
+  };
+
+  const handleDelete = () => {
+    console.log("delete button has been clicked");
+  };
+
+  const handleClose = () => {
+    console.log("close button has been clicked");
+  };
+
+  if (loading) {
+    return <div className={styles.spinner}>Loading...</div>;
+  }
+
+  if (error) {
+    return <div className={styles.error}>Error fetching jobs: {error.message}</div>;
+  }
+
+  return (
+    <section className={styles.jobs__page}>
+      <h1 className={styles.page__title}> Job Posts</h1>
+
+      <div className={styles.job__container}>
+        {jobs.length === 0 ? (
+          <p>No jobs posted yet.</p>
+        ) : (
+          <>
+            {/* Table view */}
+            <table className={styles.jobs__table}>
+              <thead>
+                <tr>
+                  <th>Job Title</th>
+                  <th>Applicants</th>
+                  <th>Date Posted</th>
+                  <th>Job Status</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {jobs.map((job) => (
+                  <tr key={job.jobId}>
+                    <td>
+                      <a href="/" className={styles.job__link}>
+                        {job.title}
+                      </a>
+                    </td>
+                    <td>{job.applicantCount || "No applicants yet"}</td>
+                    <td>{job.createdAt.toDate().toDateString()}</td>
+                    <td className={styles.job__status}>{job.active ? "Active" : "Inactive"}</td>
+                    <td>
+                      <div className={styles.dropdown}>
+                        <button onClick={() => handleMenuClick(job.jobId)} className={styles.dropdown__button}>
+                          <CiMenuKebab />
+                        </button>
+                        {activeDropdown === job.jobId && (
+                          <div className={styles.dropdown__menu}>
+                            <button onClick={() => handleEdit(job.jobId)}>Edit</button>
+                            <button onClick={() => handleDelete(job.jobId)}>Delete</button>
+                            <button onClick={() => handleClose(job.jobId)}>Close</button>
+                          </div>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            {/* Card view */}
+            <div className={styles.jobs__grid}>
+              {jobs.map((job) => (
+                <div key={job.jobId} className={styles.job__card}>
+                  <div className={styles.job_header}>
+                    <a href="/" className={styles.job__link}>
+                      {job.title}
+                    </a>
+                    <button onClick={() => handleMenuClick(job.jobId)} className={styles.dropdown__button}>
+                      <CiMenuKebab />
+                    </button>
+                    
+                  </div>
+                  <div className={styles.dropdown}>
+                    {activeDropdown === job.jobId && (
+                      <div className={styles.dropdown__menu}>
+                        <button onClick={() => handleEdit(job.jobId)}>Edit</button>
+                        <button onClick={() => handleDelete(job.jobId)}>Delete</button>
+                        <button onClick={() => handleClose(job.jobId)}>Close</button>
+                      </div>
+                    )}
+                  </div>
+                  <p>Applicants: {job.applicantCount || "No applicants yet"}</p>
+                  <p>Date Posted: {job.createdAt.toDate().toDateString()}</p>
+                  <p>Job Status: {job.active ? "Active" : "Inactive"}</p>
+                 
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default JobPage;
