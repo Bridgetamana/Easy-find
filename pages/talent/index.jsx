a126d81edbaa731b9ef9b753ef56b7985f7039fa
Lilianada <lilibest30.lb@gmail.com>
2024-10-16 15:36:57 +0100

    updates: remove Hero folder from authorized folder and changed it to TalentHero, while i tokk out the former TalentHero as it was not in use. Changed the texts in telent hero and commented out ExpectionSection

--- pages/talent/index.jsx
+++ pages/talent/index.jsx
@@ -2,20 +2,21 @@
 
 import React from "react";
 import ProtectedRoute from "@/utils/protectedRoute";
-import Hero from "@/components/authorized/Hero";
+import Hero from "@/components/authorized/TalentComponents/TalentHero";
 import AppliedJobs from "@/components/authorized/TalentComponents/AppliedJobs";
 import ExpectationSection from "@/components/authorized/TalentComponents/ExpectationSection";
 import SavedJobs from "@/components/authorized/TalentComponents/SavedJobs";
 import UserName from "@/components/authorized/TalentComponents/UserName";
 import TalentLayout from "./layout";
+import TalentHero from "@/components/authorized/TalentComponents/TalentHero";
 
 const HomePage = () => {
   return (
     <ProtectedRoute>
       <TalentLayout>
         <UserName />
-        <Hero />
-        <ExpectationSection />
+        <TalentHero />
+        {/* <ExpectationSection /> */}
         <SavedJobs />
         <AppliedJobs />
       </TalentLayout>
